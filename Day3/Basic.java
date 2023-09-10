7c032abad4c21de9505a757192911993587441a4
VishalKumar6767 <kumarvishal27697@gmail.com>
2023-09-10 19:37:34 +0530

    3rd day practice:

--- Day3/Basic.java
+++ Day3/Basic.java
@@ -10,15 +10,43 @@ public class Basic {
                 int bottom = (2 * n - 2) - i;
                 int right = (2 * n - 2) - j;
                 int count = (n - Math.min(Math.min(top, bottom), Math.min(right, left)));
-                System.out.print(count+" ");
+                System.out.print(count + " ");
             }
             System.out.println();
         }
 
     }
 
+    public static void charTriangle(int n) {
+        for (int i = 1; i <= n; i++) {
+            int ch = 65;
+            for (int j = 1; j <= i; j++) {
+                System.out.print((char) ch);
+                ch++;
+
+            }
+            System.out.println();
+        }
+    }
+
+    public static void printTriangle(int n) {
+        // code here
+        for (int i = n; i >= 1; i--) {
+            int ch = 65;
+            for (int j = 1; j <= i; j++) {
+                System.out.print((char) ch);
+                ch++;
+            }
+            System.out.println();
+        }
+    }
+
     public static void main(String[] args) {
         rectangle(4);
+        System.out.println("-----------------------------");
+        charTriangle(6);
+         System.out.println("-----------------------------");
+        printTriangle(6);
 
     }
 
