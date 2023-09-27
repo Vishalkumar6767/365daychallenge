f1120eefd75400efe14e7d4c40952c82c4d104f4
VishalKumar6767 <kumarvishal27697@gmail.com>
2023-09-27 19:11:32 +0530

    Day 9 code

--- Day9/Array.java
+++ Day9/Array.java
@@ -1,5 +1,18 @@
 package Day9;
 
 public class Array {
+    public static int largest(int arr[],int n){
+        int largest = arr[0];
+        for(int i = 0; i <= n-1;i++){
+            if(arr[i] > largest){
+                largest = arr[i];
+            }
+        }
+        return largest;
+    }
+    public static void main(String args[]){
+        int arr [] = {12,34,12,12,43,2,434,54};
+        System.out.println(largest(arr,7));
+    }
     
 }
